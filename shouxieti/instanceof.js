24984da56dbea17d50f56a4ed7491d28758ceb9d
GuoChengZhi <1265986077@qq.com>
2024-08-02 23:20:52 +0800

    feat:8/2

--- shouxieti/instanceof.js
+++ shouxieti/instanceof.js
@@ -2,7 +2,8 @@ const _instance=(obj,type)=>{
 if(typeof obj!=='object'||obj==null) return false
 
 //先找对象原型
-let proto =Object.getPrototypeOf(obj)
+//原型对象可以通过 __proto__ 属性访问，也可以通过 Object.getPrototypeOf() 方法获取
+let proto =Object.getPrototypeOf(obj)  //就是Object.prototype
 while (proto){
     
     if (proto==type.prototype) return true 
@@ -15,3 +16,14 @@ while (proto){
 console.log( _instance  ({},Object));
 console.log([],Array);
 console.log(1,Object);
+
+//大概的判断引用类型
+const Type=(obj)=>{
+let res=Object.getPrototypeOf(obj)
+return res
+}
+let x=[1,3,2,2],y=5,z=()=>{
+    console.log('11');
+}
+
+console.log(Type(z));
